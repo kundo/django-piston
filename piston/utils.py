ec81906c05c28d97a234d28bd22b4971d755e22a
Emil Stenström <em@kth.se>
2015-05-18 17:50:37 +0200

    Fix PEP8 and remove unused imports.

--- piston/utils.py
+++ piston/utils.py
@@ -1,8 +1,7 @@
 import time
 
 import django
-from django.http import HttpResponseNotAllowed, HttpResponseForbidden, HttpResponse, HttpResponseBadRequest
-from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.core.cache import cache
 from django import get_version as django_version
 from django.core.mail import send_mail, mail_admins
@@ -11,8 +10,6 @@ from django.utils.translation import ugettext as _
 from django.template import loader, TemplateDoesNotExist
 from django.contrib.sites.models import Site
 from decorator import decorator
-
-from datetime import datetime, timedelta
 
 __version__ = '0.2.3rc1'
 
@@ -27,23 +24,25 @@ class rc_factory(object):
     """
     Status codes.
     """
-    CODES = dict(ALL_OK = ('OK', 200),
-                 CREATED = ('Created', 201),
-                 ACCEPTED = ('Accepted', 202),
-                 DELETED = ('', 204), # 204 says "Don't send a body!"
-                 BAD_REQUEST = ('Bad Request', 400),
-                 FORBIDDEN = ('Forbidden', 401),
-                 NOT_FOUND = ('Not Found', 404),
-                 NOT_ACCEPTABLE = ('Not acceptable', 406),
-                 DUPLICATE_ENTRY = ('Conflict/Duplicate', 409),
-                 NOT_HERE = ('Gone', 410),
-                 INTERNAL_ERROR = ('Internal Error', 500),
-                 NOT_IMPLEMENTED = ('Not Implemented', 501),
-                 THROTTLED = ('Throttled', 503))
+    CODES = dict(
+        ALL_OK=('OK', 200),
+        CREATED=('Created', 201),
+        ACCEPTED=('Accepted', 202),
+        DELETED=('', 204),  # 204 says "Don't send a body!"
+        BAD_REQUEST=('Bad Request', 400),
+        FORBIDDEN=('Forbidden', 401),
+        NOT_FOUND=('Not Found', 404),
+        NOT_ACCEPTABLE=('Not acceptable', 406),
+        DUPLICATE_ENTRY=('Conflict/Duplicate', 409),
+        NOT_HERE=('Gone', 410),
+        INTERNAL_ERROR=('Internal Error', 500),
+        NOT_IMPLEMENTED=('Not Implemented', 501),
+        THROTTLED=('Throttled', 503)
+    )
 
     def __getattr__(self, attr):
         """
-        Returns a fresh `HttpResponse` when getting 
+        Returns a fresh `HttpResponse` when getting
         an "attribute". This is backwards compatible
         with 0.2, which is important.
         """
@@ -85,9 +84,9 @@ class rc_factory(object):
             HttpResponseWrapper = HttpResponse
 
         return HttpResponseWrapper(r, content_type='text/plain', status=c)
-    
+
 rc = rc_factory()
-    
+
 class FormValidationError(Exception):
     def __init__(self, form):
         self.form = form
@@ -100,7 +99,7 @@ def validate(v_form, operation='POST'):
     @decorator
     def wrap(f, self, request, *a, **kwa):
         form = v_form(getattr(request, operation), request.FILES)
-    
+
         if form.is_valid():
             setattr(request, 'form', form)
             return f(self, request, *a, **kwa)
@@ -108,15 +107,15 @@ def validate(v_form, operation='POST'):
             raise FormValidationError(form)
     return wrap
 
-def throttle(max_requests, timeout=60*60, extra=''):
+def throttle(max_requests, timeout=60 * 60, extra=''):
     """
     Simple throttling decorator, caches
     the amount of requests made in cache.
-    
+
     If used on a view where users are required to
     log in, the username is used, otherwise the
     IP address of the originating request is used.
-    
+
     Parameters::
      - `max_requests`: The maximum number of requests
      - `timeout`: The timeout for the cache entry (default: 1 hour)
@@ -127,7 +126,7 @@ def throttle(max_requests, timeout=60*60, extra=''):
             ident = request.user.username
         else:
             ident = request.META.get('REMOTE_ADDR', None)
-    
+
         if hasattr(request, 'throttle_extra'):
             """
             Since we want to be able to throttle on a per-
@@ -136,7 +135,7 @@ def throttle(max_requests, timeout=60*60, extra=''):
             object. If so, append the identifier name with it.
             """
             ident += ':%s' % str(request.throttle_extra)
-        
+
         if ident:
             """
             Preferrably we'd use incr/decr here, since they're
@@ -145,7 +144,7 @@ def throttle(max_requests, timeout=60*60, extra=''):
             stable, you can change it here.
             """
             ident += ':%s' % extra
-    
+
             now = time.time()
             count, expiration = cache.get(ident, (1, None))
 
@@ -159,8 +158,8 @@ def throttle(max_requests, timeout=60*60, extra=''):
                 t['Retry-After'] = wait
                 return t
 
-            cache.set(ident, (count+1, expiration), (expiration - now))
-    
+            cache.set(ident, (count + 1, expiration), (expiration - now))
+
         return f(self, request, *args, **kwargs)
     return wrap
 
@@ -170,7 +169,7 @@ def coerce_put_post(request):
     In case we send data over PUT, Django won't
     actually look at the data and load it. We need
     to twist its arm here.
-    
+
     The try/except abominiation here is due to a bug
     in mod_python. This should fix it.
     """
@@ -178,17 +177,17 @@ def coerce_put_post(request):
         # Bug fix: if _load_post_and_files has already been called, for
         # example by middleware accessing request.POST, the below code to
         # pretend the request is a POST instead of a PUT will be too late
-        # to make a difference. Also calling _load_post_and_files will result 
+        # to make a difference. Also calling _load_post_and_files will result
         # in the following exception:
         #   AttributeError: You cannot set the upload handlers after the upload has been processed.
-        # The fix is to check for the presence of the _post field which is set 
-        # the first time _load_post_and_files is called (both by wsgi.py and 
+        # The fix is to check for the presence of the _post field which is set
+        # the first time _load_post_and_files is called (both by wsgi.py and
         # modpython.py). If it's set, the request has to be 'reset' to redo
         # the query value parsing in POST mode.
         if hasattr(request, '_post'):
             del request._post
             del request._files
-        
+
         try:
             request.method = "POST"
             request._load_post_and_files()
@@ -197,7 +196,7 @@ def coerce_put_post(request):
             request.META['REQUEST_METHOD'] = 'POST'
             request._load_post_and_files()
             request.META['REQUEST_METHOD'] = 'PUT'
-            
+
         request.PUT = request.POST
 
 
@@ -209,10 +208,10 @@ class MimerDataException(Exception):
 
 class Mimer(object):
     TYPES = dict()
-    
+
     def __init__(self, request):
         self.request = request
-        
+
     def is_multipart(self):
         content_type = self.content_type()
 
@@ -230,7 +229,7 @@ class Mimer(object):
             for mime in mimes:
                 if ctype.startswith(mime):
                     return loadee
-                    
+
     def content_type(self):
         """
         Returns the content type of the request in all cases where it is
@@ -239,10 +238,10 @@ class Mimer(object):
         type_formencoded = "application/x-www-form-urlencoded"
 
         ctype = self.request.META.get('CONTENT_TYPE', type_formencoded)
-        
+
         if type_formencoded in ctype:
             return None
-        
+
         return ctype
 
     def translate(self):
@@ -253,21 +252,21 @@ class Mimer(object):
         key-value (and maybe just a list), the data will be placed on
         `request.data` instead, and the handler will have to read from
         there.
-        
+
         It will also set `request.content_type` so the handler has an easy
         way to tell what's going on. `request.content_type` will always be
         None for form-encoded and/or multipart form data (what your browser sends.)
-        """    
+        """
         ctype = self.content_type()
         self.request.content_type = ctype
-        
+
         if not self.is_multipart() and ctype:
             loadee = self.loader_for_type(ctype)
-            
+
             if loadee:
                 try:
                     self.request.data = loadee(self.request.raw_post_data)
-                        
+
                     # Reset both POST and PUT from request, as its
                     # misleading having their presence around.
                     self.request.POST = self.request.PUT = dict()
@@ -278,18 +277,18 @@ class Mimer(object):
                 self.request.data = None
 
         return self.request
-                
+
     @classmethod
     def register(cls, loadee, types):
         cls.TYPES[loadee] = types
-        
+
     @classmethod
     def unregister(cls, loadee):
         return cls.TYPES.pop(loadee)
 
 def translate_mime(request):
     request = Mimer(request).translate()
-    
+
 def require_mime(*mimes):
     """
     Decorator requiring a certain mimetype. There's a nifty
@@ -301,10 +300,12 @@ def require_mime(*mimes):
         m = Mimer(request)
         realmimes = set()
 
-        rewrite = { 'json':   'application/json',
-                    'yaml':   'application/x-yaml',
-                    'xml':    'text/xml',
-                    'pickle': 'application/python-pickle' }
+        rewrite = {
+            'json': 'application/json',
+            'yaml': 'application/x-yaml',
+            'xml': 'text/xml',
+            'pickle': 'application/python-pickle',
+        }
 
         for idx, mime in enumerate(mimes):
             realmimes.add(rewrite.get(mime, mime))
@@ -316,7 +317,7 @@ def require_mime(*mimes):
     return wrap
 
 require_extended = require_mime('json', 'yaml', 'xml', 'pickle')
-    
+
 def send_consumer_mail(consumer):
     """
     Send a consumer an email depending on what their status is.
@@ -331,20 +332,22 @@ def send_consumer_mail(consumer):
             subject += "has been canceled."
         elif consumer.status == "rejected":
             subject += "has been rejected."
-        else: 
+        else:
             subject += "is awaiting approval."
 
-    template = "piston/mails/consumer_%s.txt" % consumer.status    
-    
+    template = "piston/mails/consumer_%s.txt" % consumer.status
+
     try:
-        body = loader.render_to_string(template, 
-            { 'consumer' : consumer, 'user' : consumer.user })
+        body = loader.render_to_string(template, {
+            'consumer': consumer,
+            'user': consumer.user,
+        })
     except TemplateDoesNotExist:
-        """ 
+        """
         They haven't set up the templates, which means they might not want
         these emails sent.
         """
-        return 
+        return
 
     try:
         sender = settings.PISTON_FROM_EMAIL
@@ -361,4 +364,3 @@ def send_consumer_mail(consumer):
         print "Mail being sent, to=%s" % consumer.user.email
         print "Subject: %s" % _(subject)
         print body
-
